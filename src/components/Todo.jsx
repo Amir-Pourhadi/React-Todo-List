2404df5cd950d32998c042fa6abf93e92646f08b
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-07-31 12:54:34 +0430

    icons and edit added

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
 import { RiCloseCircleLine } from "react-icons/ri";
 import { TiEdit } from "react-icons/ti";
+import TodoForm from "./TodoForm";
 
-export default function Todo({ todos, completeTodo }) {
+export default function Todo({ todos, completeTodo, removeTodo, editTodo }) {
 	const [edit, setEdit] = useState({ id: null, value: "" });
+
+	const submitEdit = (value) => {
+		editTodo(edit.id, value);
+		setEdit({ id: null, value: "" });
+	};
+
+	if (edit.id) return <TodoForm edit={edit} addTodo={submitEdit} />;
 
 	return todos.map(({ id, content, isComplete }, index) => (
 		<div className={isComplete ? "todo-row complete" : "todo-row"} key={index}>
@@ -11,8 +19,8 @@ export default function Todo({ todos, completeTodo }) {
 				{content}
 			</div>
 			<div className="icons">
-				<RiCloseCircleLine />
-				<TiEdit />
+				<RiCloseCircleLine className="delete-icon" onClick={() => removeTodo(id)} />
+				<TiEdit className="edit-icon" onClick={() => setEdit({ id, value: content })} />
 			</div>
 		</div>
 	));
