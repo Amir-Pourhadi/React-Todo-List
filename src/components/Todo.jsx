3f07ba73cda2facfaa2bb20b57cbcfb0fa8dc329
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-08-01 19:54:59 +0430

    comments added

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -6,11 +6,15 @@ import TodoForm from "./TodoForm";
 export default function Todo({ todos, completeTodo, removeTodo, editTodo }) {
 	const [edit, setEdit] = useState({ id: null, value: "" });
 
+	/**
+	 * To edit a todo, and empty the edit input
+	 * @param {string} value input value entered by user
+	 */
 	const submitEdit = (value) => {
 		editTodo(edit.id, value);
 		setEdit({ id: null, value: "" });
 	};
-	if (edit.id) return <TodoForm edit={edit} addTodo={submitEdit} />;
+	if (edit.id) return <TodoForm edit={edit} addTodo={submitEdit} submitValue="update" inputPlaceholder="update todo" />;
 
 	return todos.map(({ id, content, isComplete }, index) => (
 		<div className={isComplete ? "todo-row complete" : "todo-row"} key={index} onClick={() => completeTodo(id)}>
